a18ebd177f9ff8aba3499e001c4542cb3efb1fc4
otaviomd12 <125404697+otaviomd12@users.noreply.github.com>
2023-05-29 16:18:14 -0300

    Add files via upload

---
+++ Operadores Matematicos.kt
@@ -0,0 +1,20 @@
+fun main() {
+  //operadores matematicos
+  var x = 13
+  var y = 4
+  var soma=x+y
+  println("A soma é:"+soma)
+  var sub=x-y
+  println("A subtracao é:"+sub) 
+  var divisao=x/y
+  println("A divisao é:"+divisao)
+  var multi=x*y
+  println("A mult é:"+multi)
+  var rest=x%y
+  println("A rest é:"+rest)
+
+  println("0 x é" + x)
+  ++X
+   println
+  
+}
