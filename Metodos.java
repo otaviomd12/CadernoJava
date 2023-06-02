af422c5db648352a3efae0a885c8042d18e26d79
otaviomd12 <125404697+otaviomd12@users.noreply.github.com>
2023-06-02 15:30:33 -0300

    Add files via upload

---
+++ Metodos.java
@@ -0,0 +1,36 @@
+class Main {
+   static void impPrime(){
+    System.out.println("Só imprime isso");
+  }
+  
+  static void meuMetodo(String nome) {
+    System.out.println("O nome é: " + nome);
+  }
+
+
+    static void nomeEidade(String nome,int ano) {
+    System.out.println(nome + " tem "+ano + " anos");
+  }
+
+    
+  public static void main (String[] args){
+
+  impPrime();
+  
+  meuMetodo("Otavio");
+  meuMetodo("Otavio");
+  meuMetodo("Otavio");
+
+  nomeEidade("Otavio", 18);
+  nomeEidade("Otavio", 18);
+  nomeEidade("Otavio", 18);
+
+                           
+  
+}
+
+
+
+
+  
+}
