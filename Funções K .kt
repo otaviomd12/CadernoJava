e4d820bd360894db08bbbdd7ebec6420b9e9e601
otaviomd12 <125404697+otaviomd12@users.noreply.github.com>
2023-06-06 17:44:24 -0300

    Add files via upload

---
+++ Funções K .kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+  var nomeVariavel: Boolean = false
+  println(nomeVariavel)
+
+  var x: Boolean = false
+  println(x)
+
+  if (x){
+    println("verdadeiro")
+   }
+  else {
+    println("falso")
+  }
+}
