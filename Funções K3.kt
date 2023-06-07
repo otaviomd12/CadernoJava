63a1fdc6742dfec74920b79b03c7ef0d983311c9
otaviomd12 <125404697+otaviomd12@users.noreply.github.com>
2023-06-07 15:57:20 -0300

    Add files via upload

---
+++ Funções K3.kt
@@ -0,0 +1,34 @@
+fun meuNome(nome:String){
+  println("Seu nome é: "+nome)
+}
+
+fun veroufalso(x:Boolean){
+  println(x)
+}
+
+fun seramaior(y:Int){
+  if(y>=18){
+    println("É maior de idade: " + y)
+  }else{
+    println("É menor de idade: " + y)
+  }
+}
+
+
+
+fun soma(a: Int, b: Int): Int {
+  return (a + b)
+}
+
+
+fun main() {
+  meuNome("Otávio")
+  veroufalso(true)
+  veroufalso(false)
+
+  seramaior(15)
+  seramaior(19)
+
+  var resultado = soma(3, 5)
+  println(resultado)
+}
